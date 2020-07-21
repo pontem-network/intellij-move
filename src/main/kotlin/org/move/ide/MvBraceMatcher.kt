206daede79613238079ef2d75598ee230fd8e0ed
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2020-07-21 16:26:49 +0300

    add a bit better highlighting and brace matching

---
+++ src/main/kotlin/org/move/ide/MvBraceMatcher.kt
@@ -0,0 +1,40 @@
+package org.move.ide
+
+import com.intellij.lang.BracePair
+import com.intellij.lang.PairedBraceMatcher
+import com.intellij.psi.PsiFile
+import com.intellij.psi.TokenType
+import com.intellij.psi.tree.IElementType
+import com.intellij.psi.tree.TokenSet
+import org.move.lang.MvElementTypes.*
+import org.move.lang.core.MV_COMMENTS
+
+class MvBraceMatcher : PairedBraceMatcher {
+    override fun getPairs() = PAIRS
+
+    override fun isPairedBracesAllowedBeforeType(lbraceType: IElementType, next: IElementType?): Boolean =
+        next in InsertPairBraceBefore
+
+    override fun getCodeConstructStart(file: PsiFile?, openingBraceOffset: Int): Int = openingBraceOffset
+
+    companion object {
+        private val PAIRS: Array<BracePair> = arrayOf(
+            BracePair(L_BRACE, R_BRACE, true /* structural */),
+            BracePair(L_PAREN, R_PAREN, false),
+            BracePair(L_BRACK, R_BRACK, false),
+            BracePair(LT, GT, false)
+        )
+
+        private val InsertPairBraceBefore = TokenSet.orSet(
+            MV_COMMENTS,
+            TokenSet.create(
+                TokenType.WHITE_SPACE,
+                SEMICOLON,
+                COMMA,
+                R_PAREN,
+                R_BRACK,
+                R_BRACE, L_BRACE
+            )
+        )
+    }
+}
--- src/main/kotlin/org/move/ide/MvHighlighter.kt
+++ src/main/kotlin/org/move/ide/MvHighlighter.kt
@@ -4,9 +4,9 @@ import com.intellij.lexer.Lexer
 import com.intellij.openapi.editor.colors.TextAttributesKey
 import com.intellij.openapi.fileTypes.SyntaxHighlighterBase
 import com.intellij.psi.tree.IElementType
+import org.move.lang.MvElementTypes.*
 import org.move.lang.core.MV_KEYWORDS
 import org.move.lang.MvLexer
-import org.move.lang.MvElementTypes
 import com.intellij.openapi.editor.DefaultLanguageHighlighterColors as Default
 
 class MvHighlighter : SyntaxHighlighterBase() {
@@ -14,12 +14,25 @@ class MvHighlighter : SyntaxHighlighterBase() {
 
     override fun getTokenHighlights(tokenType: IElementType?): Array<TextAttributesKey> {
         val color = when (tokenType) {
-            MvElementTypes.BLOCK_COMMENT -> Default.BLOCK_COMMENT
-            MvElementTypes.LINE_COMMENT -> Default.LINE_COMMENT
-            MvElementTypes.HEX_STRING_LITERAL, MvElementTypes.BYTE_STRING_LITERAL -> Default.STRING
-            MvElementTypes.INTEGER_LITERAL, MvElementTypes.ADDRESS_LITERAL -> Default.NUMBER
-            MvElementTypes.BOOL_LITERAL -> Default.KEYWORD
+            IDENTIFIER -> Default.IDENTIFIER
+
+            BLOCK_COMMENT -> Default.BLOCK_COMMENT
+            LINE_COMMENT -> Default.LINE_COMMENT
+
+            L_PAREN, R_PAREN -> Default.PARENTHESES
+            L_BRACE, R_BRACE -> Default.BRACES
+            L_BRACK, R_BRACK -> Default.BRACKETS
+
+            SEMICOLON -> Default.SEMICOLON
+            DOT -> Default.DOT
+            COMMA -> Default.COMMA
+
+            HEX_STRING_LITERAL, BYTE_STRING_LITERAL -> Default.STRING
+            INTEGER_LITERAL, ADDRESS_LITERAL -> Default.NUMBER
+
+            BOOL_LITERAL -> Default.KEYWORD
             in MV_KEYWORDS -> Default.KEYWORD
+
             else -> null
         }
         return pack(color)
