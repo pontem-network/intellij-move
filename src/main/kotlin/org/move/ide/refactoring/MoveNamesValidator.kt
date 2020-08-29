81c7fb5fe85a5a66b81639bcf472d7284a80e4f3
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2020-08-29 15:21:41 +0300

    plug-in names validator

--- src/main/kotlin/org/move/ide/refactoring/MoveNamesValidator.kt
+++ src/main/kotlin/org/move/ide/refactoring/MoveNamesValidator.kt
@@ -1,11 +1,26 @@
 package org.move.ide.refactoring
 
+import com.intellij.lang.refactoring.NamesValidator
+import com.intellij.openapi.project.Project
 import com.intellij.psi.tree.IElementType
 import org.move.lang.MoveElementTypes.IDENTIFIER
 import org.move.lang.MoveLexer
+import org.move.lang.core.MV_KEYWORDS
 
-//class MoveNamesValidator: NamesValidator {
-//}
+class MoveNamesValidator : NamesValidator {
+    override fun isKeyword(name: String, project: Project?): Boolean {
+        return getLexerType(name) in MV_KEYWORDS
+    }
+
+    override fun isIdentifier(name: String, project: Project?): Boolean = isIdentifier(name)
+
+    companion object {
+        fun isIdentifier(name: String): Boolean = when (getLexerType(name)) {
+            IDENTIFIER -> true
+            else -> false
+        }
+    }
+}
 
 fun isValidMoveVariableIdentifier(name: String): Boolean = getLexerType(name) == IDENTIFIER
 
