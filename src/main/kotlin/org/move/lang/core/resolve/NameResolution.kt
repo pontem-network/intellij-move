bf93aaa6a77fbb845fcb839e49e678428fea59f1
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2020-08-31 15:45:23 +0300

    remove NameResolution.kt

--- src/main/kotlin/org/move/lang/core/resolve/NameResolution.kt
+++
@@ -1,9 +0,0 @@
-package org.move.lang.core.resolve
-
-import org.move.lang.core.psi.MovePathExpr
-
-//fun collectPathResolveVariants(referenceName: String)
-
-//fun processPathResolveVariants(path: MovePathExpr) {
-//
-//}
