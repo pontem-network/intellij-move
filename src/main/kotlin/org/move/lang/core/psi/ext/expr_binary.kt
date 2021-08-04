eb87ffca3a31226c6e228f3cf5e14b8cde10e5c8
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2021-08-04 15:56:11 +0300

    add proper type for binary expr starting with precisionless integer

--- src/main/kotlin/org/move/lang/core/psi/ext/expr_binary.kt
+++ src/main/kotlin/org/move/lang/core/psi/ext/expr_binary.kt
@@ -3,35 +3,48 @@ package org.move.lang.core.psi.ext
 import com.intellij.lang.ASTNode
 import org.move.lang.core.psi.*
 import org.move.lang.core.types.BaseType
+import org.move.lang.core.types.IntegerType
 import org.move.lang.core.types.TypeVarsMap
+
+fun binaryExprType(exprList: List<MoveExpr>, typeVars: TypeVarsMap): BaseType? {
+    for ((i, expr) in exprList.withIndex()) {
+        val exprType = expr.resolvedType(typeVars)
+        if (exprType is IntegerType && exprType.precision == null) {
+            if (i == exprList.lastIndex) return exprType
+            continue
+        }
+        return exprType
+    }
+    return null
+}
 
 
 abstract class MovePlusExprMixin(node: ASTNode): MoveElementImpl(node), MovePlusExpr {
     override fun resolvedType(typeVars: TypeVarsMap): BaseType? {
-        return this.exprList.firstOrNull()?.resolvedType(typeVars)
+        return binaryExprType(this.exprList, typeVars)
     }
 }
 
 abstract class MoveMinusExprMixin(node: ASTNode): MoveElementImpl(node), MoveMinusExpr {
     override fun resolvedType(typeVars: TypeVarsMap): BaseType? {
-        return this.exprList.firstOrNull()?.resolvedType(typeVars)
+        return binaryExprType(this.exprList, typeVars)
     }
 }
 
 abstract class MoveMulExprMixin(node: ASTNode): MoveElementImpl(node), MoveMulExpr {
     override fun resolvedType(typeVars: TypeVarsMap): BaseType? {
-        return this.exprList.firstOrNull()?.resolvedType(typeVars)
+        return binaryExprType(this.exprList, typeVars)
     }
 }
 
 abstract class MoveDivExprMixin(node: ASTNode): MoveElementImpl(node), MoveDivExpr {
     override fun resolvedType(typeVars: TypeVarsMap): BaseType? {
-        return this.exprList.firstOrNull()?.resolvedType(typeVars)
+        return binaryExprType(this.exprList, typeVars)
     }
 }
 
 abstract class MoveModExprMixin(node: ASTNode): MoveElementImpl(node), MoveModExpr {
     override fun resolvedType(typeVars: TypeVarsMap): BaseType? {
-        return this.exprList.firstOrNull()?.resolvedType(typeVars)
+        return binaryExprType(this.exprList, typeVars)
     }
 }
