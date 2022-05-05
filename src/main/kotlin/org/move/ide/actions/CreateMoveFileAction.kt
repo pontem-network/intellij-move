59ad2b79cd90806ee63bae4048740558ba7c4a6f
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2022-05-05 15:16:32 +0300

    add first named address to the file template, or default 0x11

--- src/main/kotlin/org/move/ide/actions/CreateMoveFileAction.kt
+++ src/main/kotlin/org/move/ide/actions/CreateMoveFileAction.kt
@@ -2,14 +2,23 @@ package org.move.ide.actions
 
 import com.intellij.ide.actions.CreateFileFromTemplateAction
 import com.intellij.ide.actions.CreateFileFromTemplateDialog
+import com.intellij.ide.fileTemplates.FileTemplate
+import com.intellij.ide.fileTemplates.FileTemplateManager
+import com.intellij.ide.fileTemplates.actions.AttributesDefaults
+import com.intellij.ide.fileTemplates.ui.CreateFromTemplateDialog
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiDirectory
+import com.intellij.psi.PsiFile
+import com.intellij.util.IncorrectOperationException
+import com.jetbrains.rd.util.firstOrNull
 import org.move.ide.MoveIcons
+import org.move.lang.moveProject
+import java.util.*
 
 class CreateMoveFileAction : CreateFileFromTemplateAction(CAPTION, "", MoveIcons.MOVE),
                              DumbAware {
-    override fun getActionName(directory: PsiDirectory?, newName: String, templateName: String?) = CAPTION
+    override fun getActionName(directory: PsiDirectory, newName: String, templateName: String) = CAPTION
 
     override fun buildDialog(
         project: Project,
@@ -23,7 +32,41 @@ class CreateMoveFileAction : CreateFileFromTemplateAction(CAPTION, "", MoveIcons
             .addKind("Test", MoveIcons.MOVE, "Move Test Module")
     }
 
+    override fun createFileFromTemplate(name: String, template: FileTemplate, dir: PsiDirectory): PsiFile? {
+        val moduleName = name.removeSuffix(".move")
+        return createFileFromTemplate(dir, moduleName, template)
+    }
+
+    fun createFileFromTemplate(dir: PsiDirectory, moduleName: String, template: FileTemplate): PsiFile? {
+        val project = dir.project
+        val defaultProperties = FileTemplateManager.getInstance(project).defaultProperties
+
+        val properties = Properties(defaultProperties)
+        properties.setProperty("ADDRESS", getDefaultAddressForDirectory(dir))
+
+        val element = try {
+            CreateFromTemplateDialog(
+                project, dir, template,
+                AttributesDefaults(moduleName).withFixedName(true),
+                properties
+            ).create()
+        } catch (e: IncorrectOperationException) {
+            throw e
+        } catch (e: Exception) {
+            LOG.error(e)
+            return null
+        }
+
+        return element?.containingFile
+    }
+
+    private fun getDefaultAddressForDirectory(dir: PsiDirectory): String {
+        return dir.moveProject?.packageAddresses()?.firstOrNull()?.key
+            ?: DEFAULT_ADDRESS
+    }
+
     private companion object {
         private const val CAPTION = "Move File"
+        private const val DEFAULT_ADDRESS = "0x11"
     }
 }
---
+++ src/test/kotlin/org/move/ide/actions/CreateMoveFileActionTest.kt
@@ -0,0 +1,71 @@
+package org.move.ide.actions
+
+import com.intellij.ide.fileTemplates.FileTemplateManager
+import org.intellij.lang.annotations.Language
+import org.move.openapiext.toPsiDirectory
+import org.move.utils.tests.MvProjectTestBase
+
+class CreateMoveFileActionTest : MvProjectTestBase() {
+    fun `test create module from template`() {
+        testProject {
+            moveToml(
+                """
+            [package]
+            name = "MyPackage"
+            
+            [addresses]
+            Sender = "0x1122"        
+            """
+            )
+            sources { move("main.move", "/*caret*/") }
+        }
+
+        checkTextEquals(
+            "Move Module", "MyModule", """
+                module Sender::MyModule {}
+            """
+        )
+    }
+
+    fun `test create test module from template`() {
+        testProject {
+            moveToml(
+                """
+            [package]
+            name = "MyPackage"
+            
+            [addresses]
+            Sender = "0x1122"        
+            """
+            )
+            sources { move("main.move", "/*caret*/") }
+        }
+
+        checkTextEquals(
+            "Move Test Module", "MyModuleTests", """
+                #[test_only]
+                module Sender::MyModuleTests {}
+            """
+        )
+    }
+
+    private fun checkTextEquals(
+        templateName: String,
+        moduleName: String,
+        @Language("Move") expectedText: String
+    ) {
+        val dir = this.testProject?.rootDirectory
+            ?.findChild("sources")
+            ?.toPsiDirectory(this.project)!!
+
+        val template = FileTemplateManager.getInstance(this.project).getInternalTemplate(templateName)
+        val resultFile = CreateMoveFileAction().createFileFromTemplate(dir, moduleName, template)!!
+
+        val actual = resultFile.text.trimIndent()
+        val expected = expectedText.trimIndent()
+        check(actual == expected) {
+            "Generated text\n=========\n${actual}\n=========\n\n is not equal " +
+                    "to the expected\n\n=========\n${expected}\n=========\n"
+        }
+    }
+}
