f02bacdabe0017c099fa793a5f79d75f63828ce7
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2022-05-16 23:23:16 +0300

    infer tuple literal type

--- src/test/kotlin/org/move/lang/types/ExpressionTypesTest.kt
+++ src/test/kotlin/org/move/lang/types/ExpressionTypesTest.kt
@@ -405,4 +405,13 @@ class ExpressionTypesTest: TypificationTestCase() {
         }
     }    
     """)
+
+    fun `test tuple type`() = testExpr("""
+    module 0x1::M {
+        fun m() {
+            (1u64, 1u64);
+          //^ (u64, u64)  
+        }
+    }    
+    """)
 }
