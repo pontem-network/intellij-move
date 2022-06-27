5a4ee79346a3dd191b8d0b107bde8b33ba42c158
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2022-06-27 18:20:34 +0200

    some darcula colors

--- src/main/kotlin/org/move/ide/navigation/declaration_range_handlers.kt
+++ src/main/kotlin/org/move/ide/navigation/declaration_range_handlers.kt
@@ -5,19 +5,21 @@ import com.intellij.openapi.util.TextRange
 import org.move.lang.core.psi.MvFunction
 import org.move.lang.core.psi.MvModule
 import org.move.lang.core.psi.ext.endOffset
+import org.move.lang.core.psi.ext.getPrevNonCommentSibling
 import org.move.lang.core.psi.ext.startOffset
 
 class ModuleDeclarationRangeHandler : DeclarationRangeHandler<MvModule> {
     override fun getDeclarationRange(container: MvModule): TextRange {
         val startOffset = container.startOffset
-        val endOffset = container.moduleBlock?.endOffset ?: container.endOffset
+        val endOffset = (container.moduleBlock?.getPrevNonCommentSibling() ?: container).endOffset
         return TextRange(startOffset, endOffset)
     }
 }
 
-class FunctionDeclarationRangeHandler: DeclarationRangeHandler<MvFunction> {
+class FunctionDeclarationRangeHandler : DeclarationRangeHandler<MvFunction> {
     override fun getDeclarationRange(container: MvFunction): TextRange {
-        return TextRange(container.startOffset,
-                         container.codeBlock?.endOffset ?: container.endOffset)
+        val startOffset = container.startOffset
+        val endOffset = (container.codeBlock?.getPrevNonCommentSibling() ?: container).endOffset
+        return TextRange(startOffset, endOffset)
     }
 }
