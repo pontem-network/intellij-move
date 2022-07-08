e4b5b92700f102b47fc1667730927b7afef6c116
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2022-07-08 13:55:08 +0200

    update gradle plugin and kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,8 +28,8 @@ version = pluginVersion
 
 plugins {
     id("java")
-    kotlin("jvm") version "1.7.0"
-    id("org.jetbrains.intellij") version "1.5.2"
+    kotlin("jvm") version "1.7.10"
+    id("org.jetbrains.intellij") version "1.6.0"
     id("org.jetbrains.grammarkit") version "2021.2.2"
 }
 
