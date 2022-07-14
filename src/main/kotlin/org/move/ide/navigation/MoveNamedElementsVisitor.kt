1c01093dbb03c60a4add0eb4d39c8544b451fe3b
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2022-07-14 17:01:28 +0200

    support find symbol for test functions and consts

--- src/main/kotlin/org/move/ide/navigation/MoveNamedElementsVisitor.kt
+++ src/main/kotlin/org/move/ide/navigation/MoveNamedElementsVisitor.kt
@@ -3,14 +3,14 @@ package org.move.ide.navigation
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiRecursiveVisitor
 import org.move.lang.core.psi.*
-import org.move.lang.core.psi.ext.functions
+import org.move.lang.core.psi.ext.allFunctions
+import org.move.lang.core.psi.ext.constBindings
+import org.move.lang.core.psi.ext.consts
 import org.move.lang.core.psi.ext.structs
-import org.move.lang.core.resolve.ref.Visibility
 
 abstract class MvNamedElementsVisitor : MvVisitor(), PsiRecursiveVisitor {
-    override fun visitFile(file: PsiFile) {
-        file.acceptChildren(this)
-    }
+
+    override fun visitFile(file: PsiFile) = file.acceptChildren(this)
 
     override fun visitAddressDef(o: MvAddressDef) {
         o.addressBlock?.moduleList?.map { it.accept(this) }
@@ -19,16 +19,16 @@ abstract class MvNamedElementsVisitor : MvVisitor(), PsiRecursiveVisitor {
     override fun visitModule(o: MvModule) {
         processNamedElement(o)
 
-        val functionSignatures = o.functions(Visibility.Internal)
-        functionSignatures.map { it.accept(this) }
-
-        val structSignatures = o.structs()
-        structSignatures.map { it.accept(this) }
+        o.allFunctions().forEach { it.accept(this) }
+        o.structs().forEach { it.accept(this) }
+        o.constBindings().forEach { it.accept(this) }
     }
 
     override fun visitFunction(o: MvFunction) = processNamedElement(o)
 
     override fun visitStruct(o: MvStruct) = processNamedElement(o)
+
+    override fun visitBindingPat(o: MvBindingPat) = processNamedElement(o)
 
     abstract fun processNamedElement(element: MvNamedElement)
 }
