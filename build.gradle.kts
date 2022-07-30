40416db65e055c68016aa91a9268d6a27f6cbec5
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2022-07-30 16:47:50 +0200

    bump to 1.15.0, add support for 222, some fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.intellij.tasks.RunPluginVerifierTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.util.*
 
-val propsVersion = System.getenv("GRADLE_PROPS_VERSION") ?: "221"
+val propsVersion = System.getenv("GRADLE_PROPS_VERSION") ?: "222"
 val publishingToken = System.getenv("JB_PUB_TOKEN") ?: null
 
 val baseProperties = "base-gradle.properties"
@@ -20,7 +20,7 @@ fun prop(key: String): String = props[key].toString()
 val kotlinVersion = "1.7.10"
 
 val pluginJarName = "intellij-move-$propsVersion"
-val pluginVersion = "1.14.0"
+val pluginVersion = "1.15.0"
 val pluginGroup = "org.move"
 
 group = pluginGroup
--- src/main/kotlin/org/move/toml/completion/MoveTomlSchema.kt
+++ src/main/kotlin/org/move/toml/completion/MoveTomlSchema.kt
@@ -26,7 +26,7 @@ class MoveTomlSchema private constructor(
         tables.find { it.name == tableName }?.keys.orEmpty()
 
     fun keysForDependency(): Collection<String> =
-        listOf("git", "local", "addr_subst", "rev", "branch", "subdir")
+        listOf("addr_subst", "version", "local", "digest", "git", "rev", "subdir")
 
     companion object {
         fun parse(project: Project, @Language("TOML") example: String): MoveTomlSchema {
