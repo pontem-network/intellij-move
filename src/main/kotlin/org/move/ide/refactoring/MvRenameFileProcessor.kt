6037590bf7bf91e8f4ed0cd8c272a4bcff34d473
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2022-08-11 22:43:58 +0200

    guard against invalid rename in single-module-file rename

--- src/main/kotlin/org/move/ide/refactoring/MvRenameFileProcessor.kt
+++ src/main/kotlin/org/move/ide/refactoring/MvRenameFileProcessor.kt
@@ -1,15 +1,51 @@
 package org.move.ide.refactoring
 
+import com.intellij.lang.LangBundle
+import com.intellij.lang.LanguageNamesValidation
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.options.ConfigurationException
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.psi.PsiElement
 import com.intellij.refactoring.listeners.RefactoringElementListener
+import com.intellij.refactoring.rename.RenameDialog
 import com.intellij.refactoring.rename.RenamePsiFileProcessor
 import com.intellij.usageView.UsageInfo
 import org.move.lang.MoveFile
+import org.move.lang.MoveLanguage
+import org.move.lang.core.psi.MvModule
 import org.move.lang.core.psi.rename
 import org.move.lang.modules
 
 class MvRenameFileProcessor : RenamePsiFileProcessor() {
+    override fun createRenameDialog(
+        project: Project,
+        element: PsiElement,
+        nameSuggestionContext: PsiElement?,
+        editor: Editor?
+    ): RenameDialog {
+        return object : PsiFileRenameDialog(project, element, nameSuggestionContext, editor) {
+            override fun canRun() {
+                super.canRun()
+                val moveFile = element as? MoveFile ?: return
+                if (moveFile.singleModule() != null) {
+                    val newModuleName = FileUtil.getNameWithoutExtension(newName)
+                    val namesValidator = LanguageNamesValidation.INSTANCE.forLanguage(MoveLanguage)
+                    if (namesValidator.isKeyword(newModuleName, moveFile.project)
+                        || !namesValidator.isIdentifier(newModuleName, moveFile.project)
+                    ) {
+                        throw ConfigurationException(
+                            LangBundle.message(
+                                "dialog.message.valid.identifier",
+                                newName
+                            )
+                        )
+                    }
+                }
+            }
+        }
+    }
+
     override fun renameElement(
         element: PsiElement,
         newName: String,
@@ -18,13 +54,17 @@ class MvRenameFileProcessor : RenamePsiFileProcessor() {
     ) {
         val moveFile = element as? MoveFile ?: return
 
-        val oldModuleName = moveFile.virtualFile.nameWithoutExtension
         val newModuleName = FileUtil.getNameWithoutExtension(newName)
-
-        val module = moveFile.modules().singleOrNull() ?: return
-        if (module.name == oldModuleName) {
+        val module = moveFile.singleModule()
+        if (module != null) {
             module.rename(newModuleName)
         }
         super.renameElement(element, newName, usages, listener)
+    }
+
+    private fun MoveFile.singleModule(): MvModule? {
+        return modules()
+            .singleOrNull()
+            ?.takeIf { it.name == virtualFile.nameWithoutExtension }
     }
 }
