5c979c4a47c8d244eb79b7b07e494057934ada7e
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2022-12-05 21:52:56 +0700

    revert gradle intellij plugin to 1.9.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ version = pluginVersion
 plugins {
     id("java")
     kotlin("jvm") version "1.7.20"
-    id("org.jetbrains.intellij") version "1.10.0"
+    id("org.jetbrains.intellij") version "1.9.0"
     id("org.jetbrains.grammarkit") version "2021.2.2"
     id("net.saliman.properties") version "1.5.2"
 }
