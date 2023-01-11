24afa01bd834dfea39efde2a48e626e1978ba804
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2023-01-11 19:06:11 +0300

    inline function resolution, completion

--- src/test/kotlin/org/move/lang/resolve/ResolveFunctionTest.kt
+++ src/test/kotlin/org/move/lang/resolve/ResolveFunctionTest.kt
@@ -516,4 +516,29 @@ module 0x1::main {
     }
 }        
     """)
+
+    fun `test resolve inline function from same module`() = checkByCode("""
+module 0x1::string {
+    inline fun foreach<Element>(v: vector<Element>, f: |Element|) {}
+              //X
+    fun main() {
+        foreach(vector[1, 2, 3], |e| print(e))
+        //^
+    }
+}        
+    """)
+
+    fun `test resolve inline function from different module`() = checkByCode("""
+module 0x1::string {
+    public inline fun foreach<Element>(v: vector<Element>, f: |Element|) {}
+                      //X
+}
+module 0x1::main {
+    use 0x1::string::foreach;
+    fun main() {
+        foreach(vector[1, 2, 3], |e| print(e))
+        //^
+    }
+}
+    """)
 }
