2249851a33ba3b82ca5ee8b5465b8bbd5d2fbe16
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2023-02-03 15:54:54 +0300

    update gradle to 7.6, back to intellij-gradle-plugin 1.9.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ version = pluginVersion
 plugins {
     id("java")
     kotlin("jvm") version "1.8.10"
-    id("org.jetbrains.intellij") version "1.12.0"
+    id("org.jetbrains.intellij") version "1.9.0"
     id("org.jetbrains.grammarkit") version "2021.2.2"
     id("net.saliman.properties") version "1.5.2"
 }
@@ -146,7 +146,7 @@ allprojects {
             )
             kotlinOptions {
                 jvmTarget = kotlinJvmTarget
-                languageVersion = "1.7"
+                languageVersion = "1.8"
                 apiVersion = "1.5"
                 freeCompilerArgs = listOf("-Xjvm-default=all")
             }
