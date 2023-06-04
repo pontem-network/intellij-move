cb162c0f34cd361fc85a261c19342998780bb7e0
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2023-06-04 18:24:04 +0300

    rework folding of uses and consts

--- src/main/kotlin/org/move/ide/folding/MvFoldingBuilder.kt
+++ src/main/kotlin/org/move/ide/folding/MvFoldingBuilder.kt
@@ -5,6 +5,7 @@ import com.intellij.lang.ASTNode
 import com.intellij.lang.folding.CustomFoldingBuilder
 import com.intellij.lang.folding.FoldingDescriptor
 import com.intellij.openapi.editor.Document
+import com.intellij.openapi.editor.FoldingGroup
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiComment
@@ -12,28 +13,29 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiWhiteSpace
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.util.elementType
+import com.intellij.psi.util.nextLeaf
 import org.move.cli.settings.collapseSpecs
 import org.move.lang.MoveFile
 import org.move.lang.MoveParserDefinition.Companion.BLOCK_COMMENT
 import org.move.lang.MoveParserDefinition.Companion.EOL_DOC_COMMENT
 import org.move.lang.MvElementTypes.*
 import org.move.lang.core.psi.*
-import org.move.lang.core.psi.ext.endOffset
-import org.move.lang.core.psi.ext.getNextNonCommentSibling
-import org.move.lang.core.psi.ext.getNextNonWhitespaceSibling
-import org.move.lang.core.psi.ext.startOffset
+import org.move.lang.core.psi.ext.*
 
 class MvFoldingBuilder : CustomFoldingBuilder(), DumbAware {
     override fun getLanguagePlaceholderText(node: ASTNode, range: TextRange): String {
-        return when {
-            node.elementType == USE_STMT -> "/* uses */"
-            node.elementType == CONST -> "/* consts */"
-            node.psi is MvFunctionParameterList -> "(...)"
-            node.psi is PsiComment -> "/* ... */"
-            node.psi is MvAcquiresType -> "/* acquires */"
+        when (node.elementType) {
+            L_BRACE -> return " { "
+            R_BRACE -> return " }"
+            CONST -> return "..."
+            USE_STMT -> return "..."
+        }
+        return when (node.psi) {
+            is MvAcquiresType -> "/* acquires */"
+            is PsiComment -> "/* ... */"
+            is MvFunctionParameterList -> "(...)"
             else -> "{...}"
         }
-
     }
 
     override fun buildLanguageFoldRegions(
@@ -93,8 +95,8 @@ class MvFoldingBuilder : CustomFoldingBuilder(), DumbAware {
                 fold(o)
         }
 
-        override fun visitUseStmt(o: MvUseStmt) = foldRepeatingItems(o, usesRanges)
-        override fun visitConst(o: MvConst) = foldRepeatingItems(o, constRanges)
+        override fun visitUseStmt(o: MvUseStmt) = foldRepeatingItems(o, o.use, o.use, usesRanges)
+        override fun visitConst(o: MvConst) = foldRepeatingItems(o, o.constKw, o.constKw, constRanges)
 
         private fun fold(element: PsiElement) {
             descriptors += FoldingDescriptor(element.node, element.textRange)
@@ -128,24 +130,48 @@ class MvFoldingBuilder : CustomFoldingBuilder(), DumbAware {
             }
         }
 
-        private inline fun <reified T> foldRepeatingItems(startNode: T, ranges: MutableList<TextRange>) {
+        private inline fun <reified T: MvDocAndAttributeOwner> foldRepeatingItems(
+            startNode: T,
+            startKeyword: PsiElement,
+            endKeyword: PsiElement,
+            ranges: MutableList<TextRange>
+        ) {
             if (isInRangesAlready(ranges, startNode as PsiElement)) return
 
-            var lastNode: PsiElement? = null
-            var tmpNode: PsiElement? = startNode
+            val lastNode = startNode.rightSiblings
+                .filterNot { it is PsiComment || it is PsiWhiteSpace }
+                .takeWhile { it is T }
+                .lastOrNull() ?: return
 
-            while (tmpNode is T || tmpNode is PsiWhiteSpace) {
-                tmpNode = tmpNode.getNextNonCommentSibling()
-                if (tmpNode is T)
-                    lastNode = tmpNode
+            val trailingSemicolon = when (lastNode) {
+                is MvUseStmt -> lastNode.semicolon
+                is MvConst -> lastNode.semicolon
+                else -> null
             }
 
-            if (lastNode == startNode) return
+            val foldStartOffset = endKeyword.foldRegionStart()
+            val foldEndOffset = trailingSemicolon?.startOffset ?: lastNode.endOffset
 
-            if (lastNode != null) {
-                val range = TextRange(startNode.startOffset, lastNode.endOffset)
-                descriptors += FoldingDescriptor(startNode.node, range)
-                ranges.add(range)
+            // This condition may be false when only the leading keyword is present, but the node is malformed.
+            // We don't collapse such nodes (even though they may have attributes).
+            if (foldStartOffset < foldEndOffset) {
+                val totalRange = TextRange(startNode.startOffset, lastNode.endOffset)
+                ranges.add(totalRange)
+
+                val group = FoldingGroup.newGroup("${T::class}")
+                val primaryFoldRange = TextRange(foldStartOffset, foldEndOffset)
+                // TODO: the leading keyword is excluded from folding. This makes it pretty on display, but
+                //       disables the fold action on it (and also on the last semicolon, if present). I don't
+                //       know whether it is possible to display those tokens unfolded, but still provide fold
+                //       action. Kotlin, which implements similar import folding logic, doesn't solve that
+                //       problem.
+                descriptors += FoldingDescriptor(startNode.node, primaryFoldRange, group)
+
+                if (startNode.startOffset < startKeyword.startOffset) {
+                    // Hide leading attributes and doc comments
+                    val attrRange = TextRange(startNode.startOffset, startKeyword.startOffset)
+                    descriptors += FoldingDescriptor(startNode.node, attrRange, group, "")
+                }
             }
         }
 
@@ -160,3 +186,12 @@ private fun CodeFoldingSettings.isDefaultCollapsedNode(node: ASTNode) =
     this.COLLAPSE_IMPORTS && node.elementType == USE_STMT
             || this.COLLAPSE_DOC_COMMENTS && node.elementType == EOL_DOC_COMMENT
 //            || this.COLLAPSE_CUSTOM_FOLDING_REGIONS && node.elementType == ACQUIRES_TYPE
+
+private fun PsiElement.foldRegionStart(): Int {
+    val nextLeaf = nextLeaf(skipEmptyElements = true) ?: return endOffset
+    return if (nextLeaf.text.startsWith(' ')) {
+        endOffset + 1
+    } else {
+        endOffset
+    }
+}
