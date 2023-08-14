6ed47c4eab3d41e396ab70571dd174edbc1f0940
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2023-08-14 19:52:09 +0300

    add item type sorter to structure view

---
+++ src/main/kotlin/org/move/ide/structureView/ItemTypeSorter.kt
@@ -0,0 +1,51 @@
+package org.move.ide.structureView
+
+import com.intellij.icons.AllIcons
+import com.intellij.ide.util.treeView.smartTree.ActionPresentation
+import com.intellij.ide.util.treeView.smartTree.ActionPresentationData
+import com.intellij.ide.util.treeView.smartTree.Sorter
+import org.move.lang.core.psi.MvConst
+import org.move.lang.core.psi.MvFunction
+import org.move.lang.core.psi.MvSchema
+import org.move.lang.core.psi.MvSpecFunction
+import org.move.lang.core.psi.MvStruct
+
+class ItemTypeComparator: Comparator<MvStructureViewTreeElement> {
+    override fun compare(left: MvStructureViewTreeElement, right: MvStructureViewTreeElement): Int {
+        val leftKind = getIntKind(left)
+        val rightKind = getIntKind(right)
+        return leftKind.compareTo(rightKind)
+    }
+
+    private fun getIntKind(treeElement: MvStructureViewTreeElement): Int {
+        val element = treeElement.element
+        return when (element) {
+            is MvConst -> 0
+            is MvStruct -> 1
+            is MvFunction -> 2
+            is MvSchema -> 3
+            is MvSpecFunction -> 4
+            else -> 5
+        }
+    }
+}
+
+class ItemTypeSorter: Sorter {
+    override fun getComparator(): Comparator<*> = ItemTypeComparator()
+
+    override fun getPresentation(): ActionPresentation {
+        return ActionPresentationData(
+            "By Type",
+            null,
+            AllIcons.ObjectBrowser.SortByType
+        )
+    }
+
+    override fun isVisible(): Boolean = true
+
+    override fun getName(): String = KIND_SORTER_ID
+
+    companion object {
+        const val KIND_SORTER_ID = "KIND_SORTER"
+    }
+}
