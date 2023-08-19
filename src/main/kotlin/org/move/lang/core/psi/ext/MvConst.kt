a8ca67b3176564a845e2660a879af381fd8e8092
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2023-08-19 19:43:17 +0300

    add consts to structure view

--- src/main/kotlin/org/move/lang/core/psi/ext/MvConst.kt
+++ src/main/kotlin/org/move/lang/core/psi/ext/MvConst.kt
@@ -1,6 +1,8 @@
 package org.move.lang.core.psi.ext
 
+import com.intellij.ide.projectView.PresentationData
 import com.intellij.lang.ASTNode
+import com.intellij.navigation.ItemPresentation
 import com.intellij.psi.stubs.IStubElementType
 import org.move.ide.MoveIcons
 import org.move.lang.core.psi.MvConst
@@ -31,4 +33,14 @@ abstract class MvConstMixin : MvStubbedNamedElementImpl<MvConstStub>,
         }
 
     override fun getIcon(flags: Int): Icon? = MoveIcons.CONST
+
+    override fun getPresentation(): ItemPresentation {
+        val constType = this.typeAnnotation?.text ?: ""
+        return PresentationData(
+            "${this.name}$constType",
+            null,
+            MoveIcons.CONST,
+            null
+        )
+    }
 }
