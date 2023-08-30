b8fb49796d5acb6d793b2acc099f06350cde3dcc
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2023-08-30 18:52:59 +0300

    cleanup lang.core.psi.ext

--- src/main/kotlin/org/move/lang/core/psi/ext/MoveNativeFunctionDef.kt
+++
@@ -1,4 +0,0 @@
-package org.move.lang.core.psi.ext
-
-//val MvNativeFunctionDef.isPublic: Boolean
-//    get() = isChildExists(MvElementTypes.PUBLIC)
--- src/main/kotlin/org/move/lang/core/psi/ext/MvStructPat.kt
+++ src/main/kotlin/org/move/lang/core/psi/ext/MvStructPat.kt
@@ -2,5 +2,14 @@ package org.move.lang.core.psi.ext
 
 import org.move.lang.core.psi.MvStruct
 import org.move.lang.core.psi.MvStructPat
+import org.move.lang.core.psi.MvStructPatField
+
+val MvStructPat.patFields: List<MvStructPatField>
+    get() =
+        structPatFieldsBlock.structPatFieldList
+
+val MvStructPat.patFieldNames: List<String>
+    get() =
+        patFields.map { it.referenceName }
 
 val MvStructPat.structItem: MvStruct? get() = this.path.reference?.resolveWithAliases() as? MvStruct
