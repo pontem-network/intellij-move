c6010b0ae78b530a1c1ce1325944cf3cd899f9ce
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2024-01-15 16:38:04 +0300

    support sui git paths (#129)
    Co-authored-by: qingfengzs <jinyu_hello@163.com>

--- src/main/kotlin/org/move/cli/manifest/TomlDependency.kt
+++ src/main/kotlin/org/move/cli/manifest/TomlDependency.kt
@@ -1,5 +1,6 @@
 package org.move.cli.manifest
 
+import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 
@@ -24,15 +25,28 @@ sealed class TomlDependency {
 
         override fun localPath(): Path {
             val home = System.getProperty("user.home")
-            val dirName = dirName(repo, rev)
-            return Paths.get(home, ".move", dirName, subdir)
+            // TODO: add choice based on selected blockchain
+            val dirNameAptos = dirNameAptos(repo, rev)
+            val aptosPath = Paths.get(home, ".move", dirNameAptos, subdir)
+            if (Files.exists(aptosPath)) {
+                return aptosPath
+            } else {
+                val dirNameSui = dirNameSui(repo, rev)
+                val suiPath = Paths.get(home, ".move", dirNameSui, subdir)
+                return suiPath
+            }
         }
 
         companion object {
-            fun dirName(repo: String, rev: String): String {
+            fun dirNameAptos(repo: String, rev: String): String {
                 val sanitizedRepoName = repo.replace(Regex("[/:.@]"), "_")
-                val revName = rev.replace('/', '_')
-                return "${sanitizedRepoName}_$revName"
+                val aptosRevName = rev.replace("/", "_")
+                return "${sanitizedRepoName}_$aptosRevName"
+            }
+            fun dirNameSui(repo: String, rev: String): String {
+                val sanitizedRepoName = repo.replace(Regex("[/:.@]"), "_")
+                val suiRevName = rev.replace("/", "__")
+                return "${sanitizedRepoName}_$suiRevName"
             }
         }
     }
