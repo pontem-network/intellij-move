c1b982151ceab72bd036c2d972ec3cd937c809bc
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2024-04-02 14:15:36 +0300

    detect blockchain type automatically on project creation

--- src/main/kotlin/org/move/ide/newProject/MoveLangProjectOpenProcessor.kt
+++ src/main/kotlin/org/move/ide/newProject/MoveLangProjectOpenProcessor.kt
@@ -1,14 +1,45 @@
 package org.move.ide.newProject
 
+import com.intellij.ide.util.RunOnceUtil
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.ProjectActivity
 import com.intellij.openapi.startup.StartupManager
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.readText
 import com.intellij.platform.PlatformProjectOpenProcessor
 import com.intellij.projectImport.ProjectOpenProcessor
 import org.move.cli.Consts
+import org.move.cli.moveProjectsService
+import org.move.cli.settings.Blockchain
+import org.move.cli.settings.moveSettings
 import org.move.ide.MoveIcons
+import org.move.openapiext.rootDir
 import javax.swing.Icon
+
+class InferBlockchainTypeOnStartupActivity: ProjectActivity {
+    override suspend fun execute(project: Project) {
+        RunOnceUtil.runOnceForProject(project, ID) {
+            val moveToml = project.rootDir?.findChild("Move.toml") ?: return@runOnceForProject
+            val moveTomlText = moveToml.readText()
+            val blockchain =
+                when {
+                    moveTomlText.contains("https://github.com/MystenLabs/sui.git") -> Blockchain.SUI
+                    else -> Blockchain.APTOS
+                }
+
+            if (project.moveSettings.blockchain != blockchain) {
+                project.moveSettings.modify {
+                    it.blockchain = blockchain
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val ID = "org.move.InferBlockchainTypeOnStartupActivity"
+    }
+}
 
 /// called only when IDE opens a project from existing sources
 class MoveLangProjectOpenProcessor: ProjectOpenProcessor() {
