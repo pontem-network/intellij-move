3de633269dec9f772a3796484393eeacbc73273a
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2024-05-10 01:36:11 +0300

    resource access control name resolution

---
+++ src/test/kotlin/org/move/lang/resolve/ResolveResourceAccessSpecifiersTest.kt
@@ -0,0 +1,57 @@
+package org.move.lang.resolve
+
+import org.move.utils.tests.CompilerV2
+import org.move.utils.tests.resolve.ResolveTestCase
+
+class ResolveResourceAccessSpecifiersTest: ResolveTestCase() {
+    @CompilerV2
+    fun `test resolve type for reads`() = checkByCode("""
+        module 0x1::main {
+            struct S { field: u8 }
+                 //X
+            fun main() reads S {}
+                           //^
+        }        
+    """)
+
+    @CompilerV2
+    fun `test resolve type for write`() = checkByCode("""
+        module 0x1::main {
+            struct S { field: u8 }
+                 //X
+            fun main() writes S {}
+                            //^
+        }        
+    """)
+
+    @CompilerV2
+    fun `test resolve type for acquires`() = checkByCode("""
+        module 0x1::main {
+            struct S { field: u8 }
+                 //X
+            fun main() acquires S {}
+                              //^
+        }        
+    """)
+
+    @CompilerV2
+    fun `test resolve address function in address specifier`() = checkByCode("""
+        module 0x1::main {
+            struct S { field: u8 }
+            fun main(value: u8) reads *(make_up_address(value)) {}
+                                       //^
+            fun make_up_address(x: u8): address { @0x1 }
+                  //X
+        }        
+    """)
+
+    @CompilerV2
+    fun `test resolve module with wildcard`() = checkByCode("""
+        module 0x1::main {
+                   //X
+            struct S { field: u8 }
+            fun main(value: u8) reads 0x1::main::* {}
+                                          //^
+        }        
+    """)
+}
