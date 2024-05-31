71479b01b113ab5d26f1a7d069526c6466f32f67
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2024-05-31 21:39:52 +0300

    fix notifications

--- src/main/kotlin/org/move/ide/notifications/MvNotifications.kt
+++ src/main/kotlin/org/move/ide/notifications/MvNotifications.kt
@@ -4,6 +4,10 @@ import com.intellij.notification.NotificationGroup
 import com.intellij.notification.NotificationGroupManager
 
 object MvNotifications {
+    fun buildLogGroup(): NotificationGroup {
+        return NotificationGroupManager.getInstance().getNotificationGroup("Move Compile Log")
+    }
+
     fun pluginNotifications(): NotificationGroup {
         return NotificationGroupManager.getInstance().getNotificationGroup("Move Language")
     }
