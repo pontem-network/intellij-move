ae39c6fdbd82368d47d6e199896e5563874445bd
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2024-08-23 16:11:48 +0300

    add breadcrumbs on specs

--- src/test/kotlin/org/move/ide/MvBreadcrumbsProviderTest.kt
+++ src/test/kotlin/org/move/ide/MvBreadcrumbsProviderTest.kt
@@ -21,6 +21,42 @@ class MvBreadcrumbsProviderTest : MvTestBase() {
     """
     )
 
+    fun `test breadcrumbs in module spec`() = doTextTest(
+        """
+        module 0x1::m {
+            fun main() {}
+        }
+        spec 0x1::m {
+            spec main {
+                /*caret*/
+            }
+        }
+    """, """
+        spec 0x1::m
+        spec main
+    """
+    )
+
+    fun `test breadcrumbs for expr`() = doTextTest(
+        """
+        module 0x1::m {
+            enum S { One, Two }
+            fun main() {
+                let s: S;
+                for (i in 0..10) {
+                    /*caret*/
+                }
+            }
+        }
+    """, """
+        0x1::m
+        main()
+        {...}
+        for(i in 0..10)
+        {...}
+    """
+    )
+
     private fun doTextTest(@Language("Move") content: String, info: String) {
         InlineFile(content.trimIndent())
         val crumbs = myFixture.breadcrumbsAtCaret.joinToString(separator = "\n") { it.text }
