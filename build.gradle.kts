8128f31c06dc9a49b2454d3d6118491eed440ee6
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2024-08-30 21:52:03 +0300

    update kotlin to 2.0.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -71,7 +71,7 @@ version = pluginVersion
 
 plugins {
     id("java")
-    kotlin("jvm") version "1.9.25"
+    kotlin("jvm") version "2.0.20"
     id("org.jetbrains.intellij.platform") version "2.0.1"
     id("org.jetbrains.grammarkit") version "2022.3.2.2"
     id("net.saliman.properties") version "1.5.2"
