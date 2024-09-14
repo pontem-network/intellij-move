b7cf5fa396b87237f0810ed47de2db59fe2be198
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2024-09-14 22:13:51 +0300

    test for receiver style with enums

--- src/test/kotlin/org/move/lang/resolve/compilerV2/ReceiverStyleFunctionTest.kt
+++ src/test/kotlin/org/move/lang/resolve/compilerV2/ReceiverStyleFunctionTest.kt
@@ -234,4 +234,35 @@ class ReceiverStyleFunctionTest: ResolveTestCase() {
             }
         }        
     """)
+
+    fun `test method with enum inner item`() = checkByCode("""
+        module 0x1::m {
+            enum Inner {
+                Inner1{ x: u64 }
+                Inner2{ x: u64, y: u64 }
+            }
+            struct Box has drop {
+               x: u64
+            }
+            enum Outer {
+                None,
+                One{ i: Inner },
+                Two { i: Inner, b: Box }
+            }
+            public fun is_inner1(self: &Inner): bool {
+                        //X
+                match (self) {
+                    Inner1{x: _} => true,
+                    _ => false
+                }
+            }
+            public fun main(o: Outer) {
+                match (o) {
+                    None => false
+                    One { i } if i.is_inner1() => true,
+                                   //^
+                }
+            }
+        }        
+    """)
 }
