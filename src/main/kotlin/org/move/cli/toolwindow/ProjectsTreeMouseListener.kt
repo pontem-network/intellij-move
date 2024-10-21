195f7a1d20c7cf6552d2754b813507d214b88904
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2024-10-21 19:15:51 +0200

    navigate to psi elements in the aptos tool window

--- src/main/kotlin/org/move/cli/toolwindow/ProjectsTreeMouseListener.kt
+++ src/main/kotlin/org/move/cli/toolwindow/ProjectsTreeMouseListener.kt
@@ -8,38 +8,59 @@ import com.intellij.execution.executors.DefaultRunExecutor
 import com.intellij.openapi.actionSystem.ActionPlaces
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.impl.SimpleDataContext
+import com.intellij.openapi.diagnostic.LogLevel.WARNING
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.psi.NavigatablePsiElement
+import com.intellij.psi.PsiElement
+import org.move.cli.toolwindow.MoveProjectsTreeStructure.MoveSimpleNode
+import org.move.ide.notifications.logOrShowBalloon
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
 import javax.swing.SwingUtilities
 import javax.swing.tree.DefaultMutableTreeNode
 
-class ProjectsTreeMouseListener : MouseAdapter() {
+class ProjectsTreeMouseListener: MouseAdapter() {
     override fun mouseClicked(e: MouseEvent) {
-        if (e.clickCount < 2 || !SwingUtilities.isLeftMouseButton(e)) return
+        // trigger listener only with left double-click
+        if (!SwingUtilities.isLeftMouseButton(e) || e.clickCount < 2) return
 
-        val tree = e.source as? MoveProjectsTree ?: return
-        val node = tree.selectionModel.selectionPath
-            ?.lastPathComponent as? DefaultMutableTreeNode ?: return
-        val userObject = node.userObject
-        val function = when (userObject) {
-            is MoveProjectsTreeStructure.MoveSimpleNode.Entrypoint -> userObject.function
-            is MoveProjectsTreeStructure.MoveSimpleNode.View -> userObject.function
-            else -> return
+        val projectsTree = e.source as? MoveProjectsTree ?: return
+        val treeNode = projectsTree.selectionModel
+            .selectionPath?.lastPathComponent as? DefaultMutableTreeNode ?: return
+        val userNode = treeNode.userObject
+        when (userNode) {
+            is MoveSimpleNode.Entrypoint -> navigateToPsiElement(userNode.function)
+            is MoveSimpleNode.View -> navigateToPsiElement(userNode.function)
+            is MoveSimpleNode.Module -> navigateToPsiElement(userNode.module)
         }
+    }
 
-        val functionLocation =
+    private fun executeContextAction(psiElement: PsiElement) {
+        val psiLocation =
             try {
-                PsiLocation.fromPsiElement(function) ?: return
+                PsiLocation.fromPsiElement(psiElement) ?: return
             } catch (e: PluginException) {
                 // TODO: figure out why this exception is raised
+                LOG.logOrShowBalloon(e.message, productionLevel = WARNING)
                 return
             }
         val dataContext =
-            SimpleDataContext.getSimpleContext(Location.DATA_KEY, functionLocation)
+            SimpleDataContext.getSimpleContext(Location.DATA_KEY, psiLocation)
         val actionEvent =
             AnActionEvent.createFromDataContext(ActionPlaces.TOOLWINDOW_CONTENT, null, dataContext)
 
         val executor = DefaultRunExecutor.getRunExecutorInstance()
         RunContextAction(executor).actionPerformed(actionEvent)
+    }
+
+    private fun navigateToPsiElement(psiElement: PsiElement) {
+        val navigationElement = psiElement.navigationElement as? NavigatablePsiElement ?: return
+        if (navigationElement.canNavigate()) {
+            navigationElement.navigate(true)
+        }
+    }
+
+    companion object {
+        private val LOG = logger<ProjectsTreeMouseListener>()
     }
 }
