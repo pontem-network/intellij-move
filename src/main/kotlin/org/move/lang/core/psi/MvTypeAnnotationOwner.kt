9ad1b19c533c68e2702ccb42b233969e0f012959
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2024-10-22 03:24:59 +0200

    remove old grammar items from the bnf file

--- src/main/kotlin/org/move/lang/core/psi/MvTypeAnnotationOwner.kt
+++
@@ -1,8 +0,0 @@
-package org.move.lang.core.psi
-
-val MvTypeAnnotationOwner.type: MvType? get() = this.typeAnnotation?.type
-
-interface MvTypeAnnotationOwner : MvElement {
-
-    val typeAnnotation: MvTypeAnnotation?
-}
