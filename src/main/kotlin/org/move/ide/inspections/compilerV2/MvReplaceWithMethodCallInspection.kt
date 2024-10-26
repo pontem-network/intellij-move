f0ecd5d05f0ccd33f823f4c635fb71ccedda0631
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2024-10-26 20:53:56 +0200

    replace with method call is move2only

--- src/main/kotlin/org/move/ide/inspections/compilerV2/MvReplaceWithMethodCallInspection.kt
+++ src/main/kotlin/org/move/ide/inspections/compilerV2/MvReplaceWithMethodCallInspection.kt
@@ -2,7 +2,6 @@ package org.move.ide.inspections.compilerV2
 
 import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.codeInspection.ProblemsHolder
-import org.move.ide.inspections.MvLocalInspectionTool
 import org.move.ide.inspections.compilerV2.fixes.ReplaceWithMethodCallFix
 import org.move.lang.core.psi.*
 import org.move.lang.core.psi.ext.isMsl
@@ -15,38 +14,35 @@ import org.move.lang.core.types.ty.TyReference.Companion.isCompatibleWithAutobor
 import org.move.lang.core.types.ty.hasTyUnknown
 import org.move.lang.moveProject
 
-class MvReplaceWithMethodCallInspection: MvLocalInspectionTool() {
-    override fun buildMvVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): MvVisitor {
-        return object: MvVisitor() {
-            override fun visitCallExpr(callExpr: MvCallExpr) {
-                val function = callExpr.path.reference?.resolveFollowingAliases() as? MvFunction ?: return
-                val msl = callExpr.isMsl()
-                val inference = callExpr.inference(msl) ?: return
+class MvReplaceWithMethodCallInspection:
+    Move2OnlyInspectionBase<MvCallExpr>(MvCallExpr::class.java) {
 
-                val firstArgExpr = callExpr.valueArguments.firstOrNull()?.expr ?: return
-                val firstArgExprTy = inference.getExprType(firstArgExpr)
-                if (firstArgExprTy.hasTyUnknown) return
+    override fun visitTargetElement(element: MvCallExpr, holder: ProblemsHolder, isOnTheFly: Boolean) {
+        val function = element.path.reference?.resolveFollowingAliases() as? MvFunction ?: return
+        val msl = element.isMsl()
+        val inference = element.inference(msl) ?: return
 
-                val moveProject = callExpr.moveProject ?: return
-                val methodModule = function.module ?: return
-                val itemModule = firstArgExprTy.itemModule(moveProject) ?: return
-                if (methodModule != itemModule) return
+        val firstArgExpr = element.valueArguments.firstOrNull()?.expr ?: return
+        val firstArgExprTy = inference.getExprType(firstArgExpr)
+        if (firstArgExprTy.hasTyUnknown) return
 
-                val selfTy = function.selfParamTy(msl)
-                    ?.deepFoldTyTypeParameterWith { TyInfer.TyVar(it) } ?: return
-                if (selfTy.hasTyUnknown) return
+        val moveProject = element.moveProject ?: return
+        val methodModule = function.module ?: return
+        val itemModule = firstArgExprTy.itemModule(moveProject) ?: return
+        if (methodModule != itemModule) return
 
-                if (isCompatibleWithAutoborrow(firstArgExprTy, selfTy, msl)) {
-                    // can be converted
-                    holder.registerProblem(
-                        callExpr,
-                        "Can be replaced with method call",
-                        ProblemHighlightType.WEAK_WARNING,
-                        ReplaceWithMethodCallFix(callExpr)
-                    )
-                }
-            }
+        val selfTy = function.selfParamTy(msl)
+            ?.deepFoldTyTypeParameterWith { TyInfer.TyVar(it) } ?: return
+        if (selfTy.hasTyUnknown) return
+
+        if (isCompatibleWithAutoborrow(firstArgExprTy, selfTy, msl)) {
+            // can be converted
+            holder.registerProblem(
+                element,
+                "Can be replaced with method call",
+                ProblemHighlightType.WEAK_WARNING,
+                ReplaceWithMethodCallFix(element)
+            )
         }
     }
-
 }
