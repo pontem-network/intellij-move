7aa4fbc9f59e3e3467cabe5b73efbcf104ebaf34
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2024-10-28 23:39:12 +0100

    replace with compound operator inspection

--- src/main/kotlin/org/move/lang/core/psi/ext/MvBinaryExpr.kt
+++ src/main/kotlin/org/move/lang/core/psi/ext/MvBinaryExpr.kt
@@ -1,9 +1,12 @@
 package org.move.lang.core.psi.ext
 
 import com.intellij.lang.ASTNode
+import com.intellij.psi.PsiElement
 import org.move.lang.MvElementTypes.BINARY_OP
 import org.move.lang.core.psi.MvBinaryExpr
 import org.move.lang.core.psi.MvElementImpl
+
+val MvBinaryExpr.operator: PsiElement get() = binaryOp.operator
 
 abstract class MvBinaryExprMixin(node: ASTNode): MvElementImpl(node),
                                                  MvBinaryExpr {
