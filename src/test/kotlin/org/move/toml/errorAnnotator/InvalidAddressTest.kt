533f2fa1e66e4a8afd08959ee04e399bf10d359d
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2024-11-01 13:25:40 +0100

    refactor toml annotator tests

--- src/test/kotlin/org/move/toml/errorAnnotator/InvalidAddressTest.kt
+++ src/test/kotlin/org/move/toml/errorAnnotator/InvalidAddressTest.kt
@@ -1,24 +1,28 @@
-package org.move.toml
+package org.move.toml.errorAnnotator
 
-import org.move.utils.tests.annotation.AnnotatorTestCase
-
-class MoveTomlErrorAnnotatorTest : AnnotatorTestCase(MoveTomlErrorAnnotator::class) {
-    fun `test valid addresses`() = checkMoveTomlWarnings("""
+class InvalidAddressTest: MoveTomlAnnotatorTestBase() {
+    fun `test valid addresses`() = checkMoveTomlWarnings(
+        """
         [addresses]
         addr0 = "_"
         addr1 = "0x1"
         addr2 = "0x42"
         addr3 = "0x4242424242424242424242424242424242424242424242424242420000000000"
         addr4 = "4242424242424242424242424242424242424242424242424242420000000000"
-    """)
+    """
+    )
 
-    fun `test invalid symbols in address`() = checkMoveTomlWarnings("""
+    fun `test invalid symbols in address`() = checkMoveTomlWarnings(
+        """
         [addresses]
         addr2 = "0x<error descr="Invalid address: only hex symbols are allowed">helloworld</error>"
-    """)
+    """
+    )
 
-    fun `test address is too long`() = checkMoveTomlWarnings("""
+    fun `test address is too long`() = checkMoveTomlWarnings(
+        """
         [addresses]
         addr3 = "0x<error descr="Invalid address: no more than 64 symbols allowed">424242424242424242424242424242424242424242424242424242000000000011122</error>"
-    """)
+    """
+    )
 }
