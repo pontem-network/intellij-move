946e9b02ffd080af820603ccddeeacd40a8b4d2a
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2024-12-02 17:51:40 +0100

    243 is RR, skip two tests behaving differently on RR

--- src/test/kotlin/org/move/utils/tests/MvLightTestBase.kt
+++ src/test/kotlin/org/move/utils/tests/MvLightTestBase.kt
@@ -3,6 +3,7 @@ package org.move.utils.tests
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 
 abstract class MvLightTestBase: BasePlatformTestCase() {
+
     override fun setUp() {
         super.setUp()
 
@@ -12,4 +13,5 @@ abstract class MvLightTestBase: BasePlatformTestCase() {
         this.handleMoveV2Annotation(project)
         this.handleNamedAddressAnnotations(project)
     }
+
 }
