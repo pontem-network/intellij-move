87c028bafc39552a9985054012d8fffe5304cc81
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2024-12-20 14:21:01 +0100

    try 2.2.1 gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ version = pluginVersion
 plugins {
     id("java")
     kotlin("jvm") version "2.1.0"
-    id("org.jetbrains.intellij.platform") version "2.1.0"
+    id("org.jetbrains.intellij.platform") version "2.2.1"
     id("org.jetbrains.grammarkit") version "2022.3.2.2"
     id("net.saliman.properties") version "1.5.2"
     id("de.undercouch.download") version "5.6.0"
