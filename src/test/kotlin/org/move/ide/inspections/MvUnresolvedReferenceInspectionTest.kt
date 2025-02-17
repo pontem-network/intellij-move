1d21c9b9d8c18a07a2cbb07a1a0214dc7af18366
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2025-02-17 22:12:58 +0100

    can import consts from specs (#281)

--- src/test/kotlin/org/move/ide/inspections/MvUnresolvedReferenceInspectionTest.kt
+++ src/test/kotlin/org/move/ide/inspections/MvUnresolvedReferenceInspectionTest.kt
@@ -597,4 +597,18 @@ module 0x1::m {
             }
         }
     """)
+
+    fun `test no unresolved reference for const in spec`() = checkByText("""
+        module 0x1::features {
+            const PERMISSIONED_SIGNER: u64 = 84;
+            
+        }
+        module 0x1::m {}
+        spec 0x1::m {
+            spec fun is_permissioned_signer(): bool {
+                use 0x1::features::PERMISSIONED_SIGNER;
+                PERMISSIONED_SIGNER;
+            }
+        }
+    """)
 }
--- src/test/kotlin/org/move/lang/resolve/ResolveVariablesTest.kt
+++ src/test/kotlin/org/move/lang/resolve/ResolveVariablesTest.kt
@@ -776,4 +776,19 @@ module 0x1::string_tests {
             }
         }        
     """)
+
+    fun `test const accessible from spec functions`() = checkByCode("""
+        module 0x1::features {
+            const PERMISSIONED_SIGNER: u64 = 84;
+                   //X
+            
+        }
+        module 0x1::m {}
+        spec 0x1::m {
+            spec fun is_permissioned_signer(): bool {
+                use 0x1::features::PERMISSIONED_SIGNER;
+                PERMISSIONED_SIGNER;
+                //^
+            }
+        }    """)
 }
