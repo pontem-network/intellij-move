5c916676d8f0311c1b275fce5e3b5262bb22532c
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2025-03-01 20:45:55 +0100

    cache things

--- src/main/kotlin/org/move/lang/core/resolve/scopeEntry/ItemEntries.kt
+++ src/main/kotlin/org/move/lang/core/resolve/scopeEntry/ItemEntries.kt
@@ -1,33 +1,44 @@
 package org.move.lang.core.resolve.scopeEntry
 
+import com.intellij.openapi.util.Key
+import com.intellij.psi.util.CachedValue
 import com.intellij.psi.util.CachedValuesManager.getProjectPsiDependentCache
 import org.move.lang.core.psi.MvModule
 import org.move.lang.core.psi.ext.allNonTestFunctions
 import org.move.lang.core.psi.ext.specInlineFunctions
 import org.move.lang.core.psi.ext.tupleStructs
 import org.move.lang.core.resolve.ref.NAMES
+import org.move.utils.cache
+import org.move.utils.cacheManager
+import org.move.utils.psiCacheResult
+
+private val ITEM_ENTRIES_KEY: Key<CachedValue<List<ScopeEntry>>> = Key.create("ITEM_ENTRIES_KEY")
 
 val MvModule.itemEntries: List<ScopeEntry>
     get() {
-        return getProjectPsiDependentCache(this, MvModule::itemEntriesInner)
+        return project.cacheManager
+            .cache(this, ITEM_ENTRIES_KEY) {
+                this.psiCacheResult(this.itemEntriesInner)
+            }
     }
 
-private val MvModule.itemEntriesInner: List<ScopeEntry> get() {
-    return listOf(
-        // consts
-        this.constList.asEntries(),
+private val MvModule.itemEntriesInner: List<ScopeEntry>
+    get() {
+        return listOf(
+            // consts
+            this.constList.asEntries(),
 
-        // types
-        this.enumList.asEntries(),
-        this.schemaList.asEntries(),
-        this.structList.asEntries(),
+            // types
+            this.enumList.asEntries(),
+            this.schemaList.asEntries(),
+            this.structList.asEntries(),
 
-        // callables
-        this.allNonTestFunctions().asEntries(),
-        this.tupleStructs().mapNotNull { it.asEntry()?.copyWithNs(NAMES) },
+            // callables
+            this.allNonTestFunctions().asEntries(),
+            this.tupleStructs().mapNotNull { it.asEntry()?.copyWithNs(NAMES) },
 
-        // spec callables
-        this.specFunctionList.asEntries(),
-        this.moduleItemSpecList.flatMap { it.specInlineFunctions() }.asEntries(),
-    ).flatten()
-}
+            // spec callables
+            this.specFunctionList.asEntries(),
+            this.moduleItemSpecList.flatMap { it.specInlineFunctions() }.asEntries(),
+        ).flatten()
+    }
--- src/main/kotlin/org/move/lang/core/resolve/scopeEntry/UseSpeckEntries.kt
+++ src/main/kotlin/org/move/lang/core/resolve/scopeEntry/UseSpeckEntries.kt
@@ -1,46 +1,46 @@
 package org.move.lang.core.resolve.scopeEntry
 
-import com.intellij.psi.util.CachedValuesManager.getProjectPsiDependentCache
+import com.intellij.openapi.util.Key
+import com.intellij.psi.util.CachedValue
 import org.move.ide.inspections.imports.usageScope
 import org.move.lang.core.psi.MvPath
 import org.move.lang.core.psi.MvUseAlias
-import org.move.lang.core.psi.MvUseSpeck
 import org.move.lang.core.psi.MvUseStmt
 import org.move.lang.core.psi.NamedItemScope
 import org.move.lang.core.psi.ext.MvItemsOwner
-import org.move.lang.core.psi.ext.childOfType
-import org.move.lang.core.psi.ext.childrenOfType
 import org.move.lang.core.psi.ext.qualifier
 import org.move.lang.core.resolve.ref.ALL_NS
 import org.move.lang.core.resolve.ref.itemNs
+import org.move.utils.cache
+import org.move.utils.cacheManager
+import org.move.utils.psiCacheResult
 
-val MvItemsOwner.useSpeckEntries: List<ScopeEntry> get() {
-    return getProjectPsiDependentCache(this, MvItemsOwner::useSpeckEntriesInner)
-}
+private val USE_SPECK_ENTRIES: Key<CachedValue<List<ScopeEntry>>> = Key.create("USE_SPECK_ENTRIES")
 
-private fun MvItemsOwner.useSpeckEntriesInner(): List<ScopeEntry> {
-    val speckItems = buildList {
-        for (useStmt in useStmtList) {
-            val usageScope = useStmt.usageScope
-            useStmt.forEachLeafSpeck { speckPath, alias ->
-                add(UseSpeckItem(speckPath, alias, usageScope))
-            }
+val MvItemsOwner.useSpeckEntries: List<ScopeEntry>
+    get() {
+        val stmts = this.useStmtList
+        if (stmts.isEmpty()) return emptyList()
+        return project.cacheManager.cache(this, USE_SPECK_ENTRIES) {
+            psiCacheResult(this.useStmtList.useSpeckEntries())
         }
     }
+
+private fun List<MvUseStmt>.useSpeckEntries(): List<ScopeEntry> {
+    val speckItems = this.getUseSpeckItems()
     return buildList {
         for (speckItem in speckItems) {
             val speckPath = speckItem.speckPath
-            val alias = speckItem.alias
+            val referencedItem = speckPath.reference?.resolve()
 
-            val resolvedItem = speckPath.reference?.resolve()
-            if (resolvedItem == null) {
+            val alias = speckItem.alias
+            if (referencedItem == null) {
                 // aliased element cannot be resolved, but alias itself is valid, resolve to it
                 if (alias != null) {
-                    val referenceName = speckItem.aliasOrSpeckName ?: continue
                     // any ns is possible here
                     add(
                         ScopeEntry(
-                            referenceName,
+                            speckItem.speckName,
                             alias,
                             ALL_NS
                         )
@@ -48,56 +48,74 @@ private fun MvItemsOwner.useSpeckEntriesInner(): List<ScopeEntry> {
                 }
                 continue
             }
-            val element = alias ?: resolvedItem
-            val itemNs = resolvedItem.itemNs
-            val speckItemName = speckItem.aliasOrSpeckName ?: continue
+            val element = alias ?: referencedItem
+            val itemNs = referencedItem.itemNs
             add(
                 ScopeEntry(
-                    speckItemName,
+                    speckItem.speckName,
                     element,
                     itemNs,
-                    customItemScope = speckItem.stmtUsageScope,
+                    customItemScope = speckItem.speckUsageScope,
                 )
             )
         }
     }
 }
 
-private data class UseSpeckItem(
-    val speckPath: MvPath,
-    val alias: MvUseAlias?,
-    val stmtUsageScope: NamedItemScope
-) {
-    val aliasOrSpeckName: String?
-        get() {
-            if (alias != null) {
-                return alias.name
-            } else {
-                var n = speckPath.referenceName ?: return null
-                // 0x1::m::Self -> 0x1::m
-                if (n == "Self") {
-                    n = speckPath.qualifier?.referenceName ?: return null
+
+private fun List<MvUseStmt>.getUseSpeckItems(): List<UseSpeckItem> {
+    val useStmts = this
+    return buildList {
+        for (useStmt in useStmts) {
+            val stmtSpecks = useStmt.useSpeckLeaves
+            val usageScope = useStmt.usageScope
+            for ((speckPath, alias) in stmtSpecks) {
+                val speckName = getUseSpeckName(speckPath, alias)
+                if (speckName != null) {
+                    add(UseSpeckItem(speckPath, speckName, usageScope, alias))
                 }
-                return n
             }
-        }
-}
-
-fun interface LeafUseSpeckConsumer {
-    fun consume(path: MvPath, useAlias: MvUseAlias?): Boolean
-}
-
-fun MvUseStmt.forEachLeafSpeck(consumer: LeafUseSpeckConsumer) {
-    val rootUseSpeck = this.childOfType<MvUseSpeck>() ?: return
-    val useGroup = rootUseSpeck.useGroup
-    if (useGroup == null) {
-        // basePath is null, path is full path of useSpeck
-        val alias = rootUseSpeck.useAlias
-        if (!consumer.consume(rootUseSpeck.path, alias)) return
-    } else {
-        for (childSpeck in useGroup.childrenOfType<MvUseSpeck>()) {
-            val alias = childSpeck.useAlias
-            if (!consumer.consume(childSpeck.path, alias)) continue
         }
     }
 }
+
+private data class UseSpeckItem(
+    val speckPath: MvPath,
+    val speckName: String,
+    val speckUsageScope: NamedItemScope,
+    val alias: MvUseAlias?,
+)
+
+private fun getUseSpeckName(path: MvPath, alias: MvUseAlias?): String? {
+    return if (alias != null) {
+        alias.name
+    } else {
+        var n = path.referenceName ?: return null
+        // 0x1::m::Self -> 0x1::m
+        if (n == "Self") {
+            n = path.qualifier?.referenceName ?: return null
+        }
+        n
+    }
+}
+
+val MvUseStmt.useSpeckLeaves: List<UseSpeckLeaf>
+    get() {
+        val useStmt = this
+        return buildList {
+            val rootUseSpeck = useStmt.useSpeck ?: return@buildList
+            val useGroup = rootUseSpeck.useGroup
+            if (useGroup == null) {
+                // basePath is null, path is full path of useSpeck
+                val alias = rootUseSpeck.useAlias
+                add(UseSpeckLeaf(rootUseSpeck.path, alias))
+            } else {
+                for (childSpeck in useGroup.useSpeckList) {
+                    val childAlias = childSpeck.useAlias
+                    add(UseSpeckLeaf(childSpeck.path, childAlias))
+                }
+            }
+        }
+    }
+
+data class UseSpeckLeaf(val usePath: MvPath, val useAlias: MvUseAlias?)
